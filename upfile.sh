f6d5d536c380e5e7ff5da1d9b76eecda6cda5861
Cam S <CamSchenk676@gmail.com>
2022-12-20 13:32:35 -0600

    Added multiple file support

--- upfile.sh
+++ upfile.sh
@@ -8,43 +8,51 @@ then
 fi
 
 # Store the file path argument in a variable
-filePath=$1
+files=("$@")
 
-# Execute the curl command and store the output in a variable
-output=$(curl -s -F "file=@$filePath" https://tmpfiles.org/api/v1/upload)
+for file in "${files[@]}"; do
+    # Expand the wildcard character and store the list of files in a variable
+    expanded_files=$(ls "$file")
 
-# Check if the output contains an error message
-if echo "$output" | grep -q '"message": "Server error"'
-then
-    # Print an error message and exit
-    echo "Error: Server error"
-    exit 1
-fi
+    # Print each file in the list
+    for expanded_file in $expanded_files; do
+        # Execute the curl command and store the output in a variable
+        output=$(curl -s -F "file=@$file" https://tmpfiles.org/api/v1/upload)
 
-# Extract the URL value from the JSON output using bash string manipulation
-url=${output#*\"url\":\"}  # remove everything before "url":"
-url=${url%%\"*}  # remove everything after the first "
+        # Check if the output contains an error message
+        if echo "$output" | grep -iq '"server error"'
+        then
+            # Print an error message and exit
+            echo "Error: Server error"
+            exit 1
+        fi
 
-# Extract the 6-digit number from the URL using a regular expression
-number=$(echo "$url" | grep -oE '[0-9]{6}')
+        # Extract the URL value from the JSON output using bash string manipulation
+        url=${output#*\"url\":\"}  # remove everything before "url":"
+        url=${url%%\"*}  # remove everything after the first "
 
-# Extract the first part of the URL up to the 6-digit number using a regular expression
-firstPart=$(echo "$url" | grep -oE 'https://tmpfiles.org/[^/]*')
+        # Extract the 6-digit number from the URL using a regular expression
+        number=$(echo "$url" | grep -oE '[0-9]{6}')
 
-# Extract the last part of the URL (the filename and extension) using a regular expression
-lastPart=$(echo "$url" | grep -oE '/[^/]*$')
+        # Extract the first part of the URL up to the 6-digit number using a regular expression
+        firstPart=$(echo "$url" | grep -oE 'https://tmpfiles.org/[^/]*')
 
-# Print the first part of the URL up to the 6-digit number, without the 6-digit number itself
-printf "%s" "${firstPart%$number}"
+        # Extract the last part of the URL (the filename and extension) using a regular expression
+        lastPart=$(echo "$url" | grep -oE '/[^/]*$')
 
-# Set the text attribute to bold
-tput bold
+        # Print the first part of the URL up to the 6-digit number, without the 6-digit number itself
+        printf "%s" "${firstPart%$number}"
 
-# Print the 6-digit number in bold
-printf "%s" "$number"
+        # Set the text attribute to bold
+        tput bold
 
-# Reset the text attribute to the default
-tput sgr0
+        # Print the 6-digit number in bold
+        printf "%s" "$number"
 
-# Print the last part of the URL (the filename and extension)
-echo "$lastPart"
+        # Reset the text attribute to the default
+        tput sgr0
+
+        # Print the last part of the URL (the filename and extension)
+        echo "$lastPart"
+    done
+done
